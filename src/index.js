a457ba8dc6f9fe2e737420262388bb8f2ed9f33f
Richard Lo <richardjosonlo@gmail.com>
2018-11-10 18:21:37 +0700

    create react app boiler plate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const CityGuideApp = function CityGuideApp(){
+    return(
+        <h1>
+            Hello World!
+        </h1>
+    );
+};
+
+ReactDOM.render(
+    <CityGuideApp />,
+    document.getElementById('root')
+)
